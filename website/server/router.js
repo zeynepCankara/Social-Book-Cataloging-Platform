77f2d04e7004113895cd39daac11ea9873da4d0a
Ahmet Feyzi Halaç <ahmetfeyzihalac@gmail.com>
2021-05-15 00:54:56 +0300

    Revert "add modify router"
    This reverts commit da68e1cce27cb8d5f581f8213b4bb907efe43909.

--- website/server/router.js
+++ website/server/router.js
@@ -1,34 +1,35 @@
 import express from 'express';
 import parse from 'body-parser';
 import Connection from './connection.js';
-import { user } from './credentials.js';
 
 async function runServer() {
     const app = express();
     const port = 3001;
     const connection = new Connection();
     await connection.initDatabase();
-
+    
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
     });
-
+    
     app.listen(port, () => {
         console.log(`Listening at http://localhost:${port}`)
     })
-
+    
     /**
      * Checks the credentials of user trying to login and returns status 200 if they are valid, return 400 otherwise
      */
     app.post('/login', parse.json(), async (req, res) => {
         console.log('login', req.body);
         const { username, password, userType } = req.body;
-        const results = await connection.executeQuery(`SELECT * FROM accounts WHERE username = ${username} AND password = ${password} AND userType = ${userType};`);
-        res.status(200).send(results);
-    })
 
+        // const results = await connection.executeQuery('SELECT * FROM users...');
+
+        res.status(200).send(`testLogin with ${username} and ${password}`);
+    })
+    
     /**
      * Saves new user
      */
@@ -48,7 +49,7 @@ async function runServer() {
         res.status(200).send(results);
     })
 
-
+    
     app.post('/getFilteredBooks', parse.json(), async (req, res) => {
         console.log('getFilteredBooks', req.body);
         const { bookName, author, genre, publishYear } = req.body;
@@ -62,12 +63,10 @@ async function runServer() {
         const results = await connection.executeQuery(`SELECT * FROM Book WHERE ${whereClause};`)
         res.status(200).send(results);
     })
-
+    
     app.post('/getTrackedBooks', parse.json(), async (req, res) => {
         console.log('getTrackedBooks', req.body);
         const { username } = req.body;
-        const results = await connection.executeQuery(`SELECT * FROM Tracks NATURAL JOIN User WHERE username = ${username};`);
-        console.log(results);
         /*
         response should be in this format:
         {
@@ -83,7 +82,7 @@ async function runServer() {
             ],
             otherBookID...
         }
-
+    
         */
         res.status(200).send({
             1: [
@@ -112,8 +111,6 @@ async function runServer() {
     app.post('/getReviews', parse.json(), async (req, res) => {
         console.log('getReviews', req.body);
         const { username } = req.body;
-        const results = await connection.executeQuery(`SELECT * FROM Reviews NATURAL JOIN User WHERE username = ${username};`);
-        console.log(results);
         // Get all reviews done by specified user
 
                 /*
@@ -126,7 +123,7 @@ async function runServer() {
             },
             otherBookID...
         }
-
+    
         */
         res.status(200).send({
             1: {
@@ -140,8 +137,6 @@ async function runServer() {
     app.post('/getEditions', parse.json(), async (req, res) => {
         console.log('getEditons', req.body);
         const { bookId } = req.body;
-        const results = await connection.executeQuery('SELECT * FROM Edition;');
-        console.log(results);
         res.status(200).send([
             {
                 bookId,
@@ -166,18 +161,16 @@ async function runServer() {
 
     app.post('/startTracking', parse.json(), async (req, res) => {
         console.log('startTracking', req.body);
-        const { userId, bookId, number, publisher, format, language } = req.body; // Edition is the same format as above
-        const results = await connection.executeQuery(`INSERT INTO Tracks VALUES (${userId},  ${bookId},  ${number},  ${publisher}, ${format}, ${language});`);
-        res.status(200).send(results);
+        const { username, edition } = req.body; // Edition is the same format as above
+
+        res.status(200).send();
     })
 
     app.post('/addReview', parse.json(), async (req, res) => {
         console.log('addReview', req.body);
-        const { userId, bookId, rate, comment, date } = req.body;
+        const { username, bookId, rate, comment, date } = req.body;
 
-        const results = await connection.executeQuery(`INSERT INTO Reviews VALUES (${userId},  ${bookId},  ${rate},  ${comment}, ${date});`);
-
-        res.status(200).send(results);
+        res.status(200).send();
     })
 }
 
